3fc8b516653c94e44818a973dc850d380db5e2c0
R Shaheen <r.12345@hotmail.co.uk>
2018-11-29 08:18:06 +0000

    added a column and start looping the grid

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
 document.addEventListener('DOMContentLoaded', function () {
   // Add code here
   //const board = document.getElementById('board');
-  var div = document.createElement('div');
+  /*var div = document.createElement('div');
   div.setAttribute('class', 'row');
   document.body.appendChild(div);
+  div.setAttribute('id', 'board');
+  div.setAttribute('class', 'row');
+  document.body.appendChild(div);
+  */
+  var divRow = document.createElement('div');
+  var divCol = document.createElement('div');
+
+  divRow.setAttribute('class', 'row');
+  console.log(divRow);
+  const board = document.getElementById('board');
+  board.appendChild(divRow);
+  console.log(board);
+  document.getElementById('board');
+  let row = document.getElementsByClassName('row');
+  divCol.setAttribute('class', 'col');
+  row[0].appendChild(divCol);
+
+  for (let i = 0; i < board.length; i++) {
+
+  }
+
+  console.log("\n" + board);
+
+
 
 
 });
